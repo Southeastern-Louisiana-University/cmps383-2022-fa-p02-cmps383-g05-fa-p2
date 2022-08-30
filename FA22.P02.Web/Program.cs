cf8cf5b6bb7f1dd79d871f01ef0c7ca920ed2b3f
Cameron Bonura <cameron.bonura@selu.edu>
2022-08-30 10:39:00 -0500

    GetAll working

--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -1,5 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ProductDb>(opt => opt.UseInMemoryDatabase("ProductList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,28 +23,47 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/products", () =>
-{
-    var products = Enumerable.Range(1, 2)
-}
-)
-.WithName("GetAllProducts");
+app.MapGet("/products", async (ProductDb db) =>
+    await db.Products.ToListAsync())
+    .WithName("GetAllProducts");
 
-new ProductDto(1, "DillPickle", "Reg ol' pickle", 1.99M);
-new ProductDto(2, "DillyPickle", "A very Dilly Pickle", 2.99M);
-new ProductDto(3, "DillPickley", "A very Pickley Dill", 100.99M);
+app.MapPost("/addProduct", async (Product prod, ProductDb db) =>
+{
+    db.Products.Add(prod);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/products/{prod.Id}", prod);
+});
+
+/*app.MapDelete("/removeProduct", async (Product prod, ProductDb db) =>
+{
+    db.Products.Remove(prod);
+    await db.SaveChangesAsync();
+
+    return Results.Ok;
+}*/
+
+
 
 app.Run();
 
-internal record ProductDto(int productId, string productName, string? Description, decimal productPrice)
+class Product
 {
-    public int Id = productId;
-    public string Name = productName;
-    public string? Desc = Description;
-    public decimal Price = productPrice;
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public string? Description { get; set; }
 
+    public decimal Price { get; set; }
 }
 
+class ProductDb : DbContext
+{
+    public ProductDb(DbContextOptions<ProductDb> options)
+    : base(options) { }
+
+    public DbSet<Product> Products => Set<Product>();
+}
 //see: https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
 // Hi 383 - this is added so we can test our web project automatically. More on that later
 public partial class Program { }
